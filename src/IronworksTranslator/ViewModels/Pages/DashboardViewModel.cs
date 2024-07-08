9d761fd5dab2b19d271a863a8263dc03ed6bc613
TAE-IN Sappho KIM <sappho192@gmail.com>
2024-07-08 11:58:15 +0900

    Fix wrong bindings value

--- src/IronworksTranslator/ViewModels/Pages/DashboardViewModel.cs
+++ src/IronworksTranslator/ViewModels/Pages/DashboardViewModel.cs
@@ -8,11 +8,11 @@ namespace IronworksTranslator.ViewModels.Pages
         private bool _isTranslatorActive = false;
 
         [ObservableProperty]
-        private string _translatorToogleState = Localizer.GetString("dashboard.translator.enabled");
+        private string _translatorToogleState = Localizer.GetString("dashboard.translator.disabled");
         [ObservableProperty]
-        private string _translatorToogleStateDescription = Localizer.GetString("dashboard.translator.enabled.description");
+        private string _translatorToogleStateDescription = Localizer.GetString("dashboard.translator.disabled.description");
         [ObservableProperty]
-        private string _translatorIcon = "DesktopSpeaker20";
+        private string _translatorIcon = "DesktopSpeakerOff20";
 
         [TraceMethod]
         [RelayCommand]
@@ -20,15 +20,15 @@ namespace IronworksTranslator.ViewModels.Pages
         {
             if (IsTranslatorActive)
             {
-                TranslatorToogleState = Localizer.GetString("dashboard.translator.disabled");
-                TranslatorToogleStateDescription = Localizer.GetString("dashboard.translator.disabled.description");
-                TranslatorIcon = "DesktopSpeakerOff20";
-            }
-            else
-            {
                 TranslatorToogleState = Localizer.GetString("dashboard.translator.enabled");
                 TranslatorToogleStateDescription = Localizer.GetString("dashboard.translator.enabled.description");
                 TranslatorIcon = "DesktopSpeaker20";
+            }
+            else
+            {
+                TranslatorToogleState = Localizer.GetString("dashboard.translator.disabled");
+                TranslatorToogleStateDescription = Localizer.GetString("dashboard.translator.disabled.description");
+                TranslatorIcon = "DesktopSpeakerOff20";
             }
         }
     }
